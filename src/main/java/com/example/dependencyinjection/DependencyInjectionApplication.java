2a9dc67eebf5e74cfe2c6a064cfcaef9feb0a105
Ajay Kumar <aky22896@gmail.com>
2023-12-31 13:40:35 +0530

    dependency Injection

---
+++ src/main/java/com/example/dependencyinjection/DependencyInjectionApplication.java
@@ -0,0 +1,16 @@
+package com.example.dependencyinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(DependencyInjectionApplication.class, args);
+
+
+	}
+
+}
